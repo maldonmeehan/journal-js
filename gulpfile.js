cb9e8c714a38c9c0134a8c8552d0e49ca1a99cb0
Maldon <maldonmeehan@gmail.com>
2016-08-02 07:34:49 -0700

    add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var buildProduction = utilities.env.production;
 var del = require('del');
+var jshint = require('gulp-jshint');
 
 
 gulp.task('jsBrowserify', ['concatInterface'], function() {
@@ -37,4 +38,10 @@ gulp.task("build", ['clean'], function(){
   } else {
     gulp.start('jsBrowserify');
   }
+});
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
